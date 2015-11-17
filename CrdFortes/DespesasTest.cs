b9bbe33e70d1d4ef36dd3ceacec1e43f8ddfa945
Gentil <ntgentil@gmail.com>
2015-11-17 12:13:43 -0300

    no message

--- CrdFortes/DespesasTest.cs
+++ CrdFortes/DespesasTest.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Configuration;
-using System.Net.Http;
-using CrdFortes.Application;
 using CrdFortes.Domain.Entities;
 using CrdFortes.Infra.Data.Repositories;
-using CrdFortes.MVC.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
